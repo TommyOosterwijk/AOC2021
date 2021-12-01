d4baadd71c75d44e2f92ee2ac5fc308c2c7c2b5a
tommy <t.oosterwijk@flusso.nl>
2021-12-01 12:17:40 +0100

    Init + day1

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import days.Day1;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        System.out.println("Year 2021");
+        Day1 day1 = new Day1();
+    }
+}
