ba8b2ec464cbd58cbdad4472e0781bbaf9ba9fc3
tommy <t.oosterwijk@flusso.nl>
2021-12-09 20:54:13 +0100

    Remove old cold

--- src/main/java/days/Day9.java
+++ src/main/java/days/Day9.java
@@ -11,7 +11,6 @@ public class Day9 implements DayInterface {
 
     AocUtils utils = new AocUtils();
     List<Integer> heightMap = new ArrayList<>();
-    Boolean[][] map;
 
     int maxX = 0;
     int maxY;
@@ -28,9 +27,7 @@ public class Day9 implements DayInterface {
         maxY = heightMap.size() / maxX;
 
         getAnswerA();
-        map = new Boolean[maxY][maxX];
-        //getAnswerB();
-        getAnswerBAgain();
+        getAnswerB();
     }
 
     @Override
@@ -77,101 +74,20 @@ public class Day9 implements DayInterface {
 
     @Override
     public void getAnswerB() {
-        List<Integer> basinSizeCounter = new ArrayList<>();
-
-        for(int y = 0; y < maxY; y++) {
-            for (int x = 0; x < maxX; x++) {
-                if(map[y][x] == null) {
-                    map[y][x] = true;
-                    int value = heightMap.get((y * maxX) + x);
-                    if(value != 9){
-                        int basinCounter = 1;
-
-                        basinCounter += basin(y - 1, x, value);
-                        basinCounter += basin(y + 1, x, value);
-                        basinCounter += basin(y, x - 1, value);
-                        basinCounter += basin(y, x + 1, value);
-
-
-                        if (basinCounter > 1) {
-                            basinSizeCounter.add(basinCounter);
-                        } else {
-                            map[y][x] = null;
-                        }
-                    } else {
-                        map[y][x] = false;
-                    }
-                }
-            }
-        }
-        List<Integer> result = basinSizeCounter.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        //System.out.println(result);
-        int resultB = 0;
-        for(int i = 0; i < result.size(); i++) {
-            System.out.println(result.get(i));
-            resultB += result.get(i);
-        }
-        System.out.println("B) = "+ resultB);
-
-//        for(int y = 0; y < maxY; y++) {
-//            for (int x = 0; x < maxX; x++) {
-//                System.out.print( map[y][x]+" ");
-//            }
-//            System.out.println();
-//        }
-    }
-
-    private int basin(int y, int x, int neighborValue) {
-        int result = 0;
-        if(y >= 0 && y < maxY && x >= 0 && x < maxX) {
-            if(map[y][x] == null) {
-                map[y][x] = true;
-                int value = heightMap.get((y * maxX) + x);
-
-                if(value == 9) {
-                    map[y][x] = false;
-                } else {
-                    if (Math.abs(value - neighborValue) == 1) {
-
-                        result += basin(y - 1, x, value);
-                        result += basin(y + 1, x, value);
-                        result += basin(y, x - 1, value);
-                        result += basin(y, x + 1, value);
-                        result++;
-                    } else {
-                        map[y][x] = null;
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-    public void getAnswerBAgain() {
         ArrayList<Integer> sizes = new ArrayList<>();
         ArrayList<String> validate = new ArrayList<>();
 
         for(int y = 0; y < maxY; y++) {
             for (int x = 0; x < maxX; x++) {
                 ArrayList<String> list = new ArrayList<>();
-                int value = getValueFromMap(y, x);
-                if(value != 9) {
+                if(getValueFromMap(y, x) != 9) {
 
                     String position = "y" + y + "x" + x;
 
                     if (!validate.contains(position)) {
-
-                        list.add(position);
-                        list = getBasin(y - 1, x, value, list);
-                        list = getBasin(y + 1, x, value, list);
-                        list = getBasin(y, x - 1, value, list);
-                        list = getBasin(y, x + 1, value, list);
-
+                        list = getBasin(y,x, list);
                         validate.addAll(list);
-
-                        if (list.size() > 1) {
-                            sizes.add(list.size());
-                        }
+                        sizes.add(list.size());
                     }
                 }
             }
@@ -179,22 +95,21 @@ public class Day9 implements DayInterface {
 
         sizes.sort(Comparator.reverseOrder());
 
-        System.out.println("B) new = "+ sizes.get(0) * sizes.get(1) * sizes.get(2));
+        System.out.println("B)  = "+ sizes.get(0) * sizes.get(1) * sizes.get(2));
     }
 
-    private ArrayList<String> getBasin(int y, int x, int previousValue, ArrayList<String> basinList) {
+    private ArrayList<String> getBasin(int y, int x, ArrayList<String> basinList) {
 
         if(y >= 0 && y < maxY && x >= 0 && x < maxX) {
             String position = "y"+y+"x"+x;
             if(!basinList.contains(position)) {
-                int value = getValueFromMap(y, x);
-                if(value != 9) {
+                if(getValueFromMap(y, x) != 9) {
                     basinList.add(position);
 
-                    basinList = getBasin(y-1, x, value, basinList);
-                    basinList = getBasin(y+1, x, value, basinList);
-                    basinList = getBasin(y, x-1, value, basinList);
-                    basinList = getBasin(y, x+1, value, basinList);
+                    basinList = getBasin(y-1, x, basinList);
+                    basinList = getBasin(y+1, x, basinList);
+                    basinList = getBasin(y, x-1, basinList);
+                    basinList = getBasin(y, x+1, basinList);
 
                 }
             }
