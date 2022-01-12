1e7d23caa75e9734d092011eb355a1f23128af5b
tommy <t.oosterwijk@flusso.nl>
2022-01-12 23:31:16 +0100

    Day22, B (finally)

--- src/main/java/days/Day22.java
+++ src/main/java/days/Day22.java
@@ -3,6 +3,7 @@ package days;
 import utils.AocUtils;
 
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,81 +13,15 @@ import java.util.Scanner;
 public class Day22 implements DayInterface {
 
     AocUtils utils = new AocUtils();
-    boolean[][][] grid = new boolean[101][101][101];
-    int offset = 50;
 
     public Day22() throws FileNotFoundException, URISyntaxException {
 //        System.out.println("Day22!");
-//        initGrid();
-//
-//        Scanner scanner = utils.getScannerFromFileName("day22.txt");
-//
-//        while(scanner.hasNextLine()) {
-//            String line = scanner.nextLine();
-//            boolean setQubeOn = true;
-//            String[] parts = line.substring(3).split(",");
-//            if(line.substring(0,3).equals("off")) {
-//                setQubeOn = false;
-//                parts = line.substring(4).split(",");
-//            }
-//            int[] partX = Arrays.stream(parts[0].substring(2).split("\\..")).mapToInt(Integer::parseInt).sorted().toArray();
-//            int[] partY = Arrays.stream(parts[1].substring(2).split("\\..")).mapToInt(Integer::parseInt).sorted().toArray();
-//            int[] partZ = Arrays.stream(parts[2].substring(2).split("\\..")).mapToInt(Integer::parseInt).sorted().toArray();
-//
-//            for(int z = partZ[0]; z <= partZ[1]; z++) {
-//                for(int y = partY[0]; y <= partY[1]; y++) {
-//                    for(int x = partX[0]; x <= partX[1]; x++) {
-//                        grid[z + offset ][y + offset][x + offset] = setQubeOn;
-//                    }
-//                }
-//            }
-//        }
-//        getAnswerA();
-        getAnswerB();
-    }
-
-    void initGrid() {
-        for(int z = 0; z < grid.length; z++) {
-            for(int y = 0; y < grid[z].length; y++) {
-                for(int x = 0; x < grid[z][y].length; x++) {
-                    grid[z][y][x] = false;
-                }
-            }
-        }
-    }
-    @Override
-    public void getAnswerA() {
-        int counter = 0;
-        for(int z = 0; z < grid.length; z++) {
-            for(int y = 0; y < grid[z].length; y++) {
-                for(int x = 0; x < grid[z][y].length; x++) {
-                    if(grid[z][y][x]) {
-                        counter++;
-                    }
-                }
-            }
-        }
-
-        System.out.println("A) " + counter);
-    }
-
-    @Override
-    public void getAnswerB() {
-        try {
-            startAgain();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
-
-    void startAgain() throws FileNotFoundException, URISyntaxException {
         Scanner scanner = utils.getScannerFromFileName("day22.txt");
 
         List<Cuboid> cubuidList = new ArrayList<>();
 
         while(scanner.hasNextLine()) {
+            List<Cuboid> partialCubiodList = new ArrayList<>();
             String line = scanner.nextLine();
             boolean setQubeOn = true;
             String[] parts = line.substring(3).split(",");
@@ -98,60 +33,84 @@ public class Day22 implements DayInterface {
             int[] partY = Arrays.stream(parts[1].substring(2).split("\\..")).mapToInt(Integer::parseInt).sorted().toArray();
             int[] partZ = Arrays.stream(parts[2].substring(2).split("\\..")).mapToInt(Integer::parseInt).sorted().toArray();
 
-            Cuboid cubiod = new Cuboid(partX[0],partX[1],partY[0],partY[1],partZ[0],partZ[1]);
+            Cuboid c = new Cuboid(partX[0],partX[1],partY[0],partY[1],partZ[0],partZ[1]);
 
-            for(Cuboid existingCubiod : cubuidList) {
-                boolean rightSide =false, leftSide = false, frontSide = false, backSide = false, top = false, below = false, full = false;
+            for(int i = cubuidList.size()-1; i >= 0; i--) {
+                Cuboid e = cubuidList.get(i);
 
+                if(c.xMin <= e.xMin && c.xMax >= e.xMax && c.yMin <= e.yMin && c.yMax >= e.yMax && c.zMin <= e.zMin && c.zMax >= e.zMax) {
+                    //Old cube fully fits in new cube.
+                    cubuidList.remove(i);
+                } else {
+                    if((c.xMin <= e.xMin && c.xMax >= e.xMin || c.xMax >= e.xMax && c.xMin <= e.xMax || c.xMin >= e.xMin && c.xMax <= e.xMax)
+                            && (c.yMin <= e.yMin && c.yMax >= e.yMin || c.yMax >= e.yMax && c.yMin <= e.yMax || c.yMin >= e.yMin && c.yMax <= e.yMax)
+                            && (c.zMin <= e.zMin && c.zMax >= e.zMin || c.zMax >= e.zMax && c.zMin <= e.zMax || c.zMin >= e.zMin && c.zMax <= e.zMax)) {
+                        if(e.xMin < c.xMin) {
+                            Cuboid t = new Cuboid(e.xMin, c.xMin-1, e.yMin, e.yMax, e.zMin, e.zMax);
+                            partialCubiodList.add(t);
 
-                if(existingCubiod.xMax > cubiod.xMin && existingCubiod.xMax < cubiod.xMax) {
-                    rightSide = true;
-                    System.out.println("rightSide " + rightSide);
+                            e.xMin = c.xMin;
+                        }
+                        if( e.xMax > c.xMax) {
+                            Cuboid t = new Cuboid(c.xMax +1, e.xMax, e.yMin, e.yMax, e.zMin, e.zMax);
+                            partialCubiodList.add(t);
+
+                            e.xMax = c.xMax;
+                        }
+
+                        if(e.yMin < c.yMin) {
+                            Cuboid t = new Cuboid(e.xMin, e.xMax, e.yMin, c.yMin-1, e.zMin, e.zMax);
+                            partialCubiodList.add(t);
+
+                            e.yMin = c.yMin;
+                        }
+                        if( e.yMax > c.yMax) {
+                            Cuboid t = new Cuboid(e.xMin, e.xMax, c.yMax +1, e.yMax, e.zMin, e.zMax);
+                            partialCubiodList.add(t);
+
+                            e.yMax = c.yMax;
+                        }
+
+                        if(e.zMin < c.zMin) {
+                            Cuboid t = new Cuboid(e.xMin, e.xMax, e.yMin, e.yMax, e.zMin, c.zMin-1);
+                            partialCubiodList.add(t);
+
+                            e.zMin = c.zMin;
+                        }
+                        if( e.zMax > c.zMax) {
+                            Cuboid t = new Cuboid(e.xMin, e.xMax, e.yMin, e.yMax, c.zMax +1, e.zMax);
+                            partialCubiodList.add(t);
+
+                            e.zMax = c.zMax;
+                        }
+                        cubuidList.remove(i);
+                    }
 
                 }
-
-                if(existingCubiod.xMin < cubiod.xMax && existingCubiod.xMin > cubiod.xMin) {
-                    leftSide = true;
-                    System.out.println("leftSide " + leftSide);
-
-                }
-
-                if(existingCubiod.yMax > cubiod.yMin && existingCubiod.yMax < cubiod.yMax) {
-                    top = true;
-                    System.out.println("top " + top);
-
-                }
-
-                if(existingCubiod.yMin < cubiod.yMax && existingCubiod.yMin > cubiod.yMin) {
-                    below = true;
-                    System.out.println("below " + below);
-
-                }
-
-                if(existingCubiod.zMax > cubiod.zMin && existingCubiod.zMax < cubiod.zMax) {
-                    backSide = true;
-                    System.out.println("backSide " + backSide);
-
-                }
-
-                if(existingCubiod.zMin < cubiod.zMax && existingCubiod.zMin > cubiod.zMin) {
-                    frontSide = true;
-                    System.out.println("frontSide " + frontSide);
-
-                }
-
-                if(!(existingCubiod.zMin > cubiod.zMax) && !(existingCubiod.zMax < cubiod.zMin) && !(existingCubiod.xMin > cubiod.xMax) && !(existingCubiod.xMax < cubiod.xMin) && !(existingCubiod.yMin > cubiod.yMax) && !(existingCubiod.yMax < cubiod.yMin)) {
-                    full = true;
-                    System.out.println("full match" + full);
-                }
-
-
             }
-            cubuidList.add(cubiod);
-            System.out.println();
+            if(setQubeOn) {
+                cubuidList.add(c);
+            }
+
+            cubuidList.addAll(partialCubiodList);
         }
 
-        System.out.println(cubuidList.size());
+        BigInteger result = new BigInteger("0");
+
+        for(Cuboid c : cubuidList) {
+            result = result.add(BigInteger.valueOf(Math.abs(c.xMax - c.xMin +1)).multiply( BigInteger.valueOf(Math.abs(c.yMax - c.yMin + 1))).multiply( BigInteger.valueOf(Math.abs(c.zMax - c.zMin + 1))));
+        }
+
+        System.out.println("Day 22: " + result);
+        getAnswerB();
+    }
+    @Override
+    public void getAnswerA() {
+    }
+
+    @Override
+    public void getAnswerB() {
+
     }
 }
 
